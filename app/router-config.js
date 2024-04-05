0d2bc6d8a179c060c113999879acd588adfdac7d
Emma Wood <emmawood.coder@gmail.com>
2024-04-05 12:51:50 -0600

    set up controller files

--- app/router-config.js
+++ app/router-config.js
@@ -1,32 +1,25 @@
 import { AccountController } from "./controllers/AccountController.js";
-import { HomeController } from "./controllers/HomeController.js";
+import { ClockController } from "./controllers/ClockController.js";
+import { ImagesController } from "./controllers/ImagesController.js";
+import { QuotesController } from "./controllers/QuotesController.js";
+import { TodosController } from "./controllers/TodosController.js";
+import { WeatherController } from "./controllers/WeatherController.js";
 import { AuthGuard } from "./services/AuthService.js";
 import { Router } from "./utils/Router.js";
 
 
 export const router = new Router([
-  {
-    path: '',
-    controllers: [HomeController],
-    view: /*html*/`
-    <div class="bg-white p-3">
-      <div class="card-body">
-        <p>Home Page</p>
-        <button class="btn btn-dark" onclick="app.HomeController.testButton()">😎</button>
-      </div>
-    </div>
-    `
-  },
-  {
-    path: '#/about',
-    view: 'app/views/AboutView.html'
-  },
-  {
-    path: '#/account',
-    middleware: [AuthGuard],
-    controllers: [AccountController],
-    view: 'app/views/AccountView.html',
-  }
+	{
+		path: '',
+		controllers: [ImagesController, QuotesController, WeatherController, ClockController, TodosController],
+		view: 'app/views/InspirationView.html',
+	},
+	{
+		path: '#/account',
+		middleware: [AuthGuard],
+		controllers: [AccountController],
+		view: 'app/views/AccountView.html',
+	}
 ])
 
 
